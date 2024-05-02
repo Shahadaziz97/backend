d7fdabd5390c704c5b54ff36bdb99015d0e8cea7
mariyyah <ma_ria111@icloud.com>
2024-05-02 17:44:56 +0300

    New branch

--- src/Databases/DatabaseContext.cs
+++ src/Databases/DatabaseContext.cs
@@ -8,6 +8,7 @@ public class DatabaseContext : DbContext
 {
 
     public DbSet<Stock> Stock { get; set; }
+    public DbSet<Product> Product { get; set; }
     private IConfiguration _config;
 
     public DatabaseContext(IConfiguration config)
@@ -18,25 +19,4 @@ public class DatabaseContext : DbContext
      => optionsBuilder.UseNpgsql(@$"Host={_config["Db:Host"]};Username={_config["Db:Username"]};Password={_config["Db:Password"]};Database={_config["Db:Database"]}").UseSnakeCaseNamingConvention().UseLowerCaseNamingConvention();
 
 
-    // Guid ID = Guid.NewGuid();
-    // Guid ID1 =Guid.NewGuid();
-    // Guid ID2=Guid.NewGuid();
-    // Guid ID3 =Guid.NewGuid();
-    // Guid ID4 =Guid.NewGuid();
-    // Guid ID5 =Guid.NewGuid();
-    // Guid ID6 =Guid.NewGuid();
-    // Guid ID7 =Guid.NewGuid();
-    // public DatabaseContext()
-    // {
-
-    //     stock = [
-    //         new Stock(30,150,"Red",'M'),
-    //         new Stock(20,180,"BLACK",'L'),
-    //         new Stock(40,230,"Blue",'M'),
-    //         new Stock(15,650,"black",'S'),
-    //         new Stock(22,350,"White",'L'),
-    //         // new Stock(ID,54,750,"Yellow",'S'),
-    //         // new Stock(ID,34,250,"green",'M'),
-    //     ];
-    // }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,5 @@
 using sda_onsite_2_csharp_backend_teamwork.src.Abstractions;
-using sda_onsite_2_csharp_backend_teamwork.src.DataBase;
+using sda_onsite_2_csharp_backend_teamwork.src.Databases;
 using sda_onsite_2_csharp_backend_teamwork.src.Repositories;
 using sda_onsite_2_csharp_backend_teamwork.src.Repository;
 using sda_onsite_2_csharp_backend_teamwork.src.services;
